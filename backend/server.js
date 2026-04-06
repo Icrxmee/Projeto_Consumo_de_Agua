f25a77e49783416d1e21d09ef645b030c1446018
Pedro Icaro <josepedroicaro@gmail.com>
2026-04-06 20:33:20 -0300

    Adicionando Front end

--- backend/server.js
+++ backend/server.js
@@ -149,8 +149,10 @@ db.query(sql, (err, result) => {
 
 })
 
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log("Servidor Rodando na Porta 3000")
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
 })
+
 
